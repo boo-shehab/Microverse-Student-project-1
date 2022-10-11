c99d3927ab8632fcf32b2619e6c32a9adb5f6181
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-10-11 09:56:39 +0100

    Modifying javascript

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const burgerButton = document.querySelector(' .burger-button');
 burgerButton.addEventListener('click', () => { burgerButton.classList.toggle('active'); });
 const menuLinkes = document.querySelectorAll('header nav ul.linkes li');
 for (let i = 0; i < menuLinkes.length; i += 1) {
-  menuLinkes[i].addEventListener('click', () => { burgerButton.classList.toggle('active'); });
+  menuLinkes[i].addEventListener('click', () => { burgerButton.classList.remove('active'); });
 }
